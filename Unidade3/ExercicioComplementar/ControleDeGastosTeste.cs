ea51ec42ca2793c9d8389fc2a0daf905e13fb975
yagoroberto <yago.neto@hotmail.com>
2014-08-12 09:43:26 -0300

    Exercicio Vera

--- Unidade3/ExercicioComplementar/ControleDeGastosTeste.cs
+++ Unidade3/ExercicioComplementar/ControleDeGastosTeste.cs
@@ -3,63 +3,65 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
 
 namespace Unidade3.ExercicioComplementar
 {
     class ControleDeGastosTeste
     {
-        private static void Main()
+        public static void Main()
         {
-
-            ControleDeGastos[] controleGastos = new ControleDeGastos[10];
-
-            int opcao = -1;
+            List<ControleDeGastos> controleGastos = new List<ControleDeGastos>();
+            
+            string opcao = "-1";
             int contador = -1;
-            while (opcao != 0)
+            while (opcao != "0")
             {
                 Console.WriteLine("");
-                Console.WriteLine("----- Selecione sua opção -----");
+                Console.WriteLine("--------- Selecione sua opção ---------");
                 Console.WriteLine("1 - Cadastrar");
                 Console.WriteLine("2 - Listar");
                 Console.WriteLine("0 - Sair");
-                opcao = Convert.ToInt32(Console.ReadLine());
+                opcao = Console.ReadLine();
                 switch (opcao)
                 {
-                    case 1:
+                    case "1":
+                        contador++;
+                        ControleDeGastos controleGasto = new ControleDeGastos();
                         Console.WriteLine("");
-                        Console.WriteLine("----- Cadastrar -----");
-                        Console.WriteLine("-------- Controle de gastos ---------");
+                        Console.WriteLine("--------- Cadastrar ---------");
                         Console.Write("Informe o tipo de gasto: ");
-                        controleGastos[contador].tipoDeGasto = Console.ReadLine();
+                        controleGasto.tipoDeGasto = Console.ReadLine();
                         Console.Write("Informe a data da compra: ");
-                        controleGastos[contador].dataGasto = Console.ReadLine();
+                        controleGasto.dataGasto = Console.ReadLine();
                         Console.Write("Informe o valor cobrado: ");
-                        controleGastos[contador].valorGasto = Convert.ToDouble(Console.ReadLine());
+                        controleGasto.valorGasto = Convert.ToDouble(Console.ReadLine());
                         int formaPagamento = -1;
                         while(formaPagamento != 0)
                         {
+                            Console.WriteLine("");
                             Console.WriteLine("Informe a forma de pagamento: ");
                             Console.WriteLine("1 - Dinheiro");
                             Console.WriteLine("2 - Cheque");
                             Console.WriteLine("3 - Cartão");
                             Console.WriteLine("4 - Cheque pré");
-                            controleGastos[contador].formaDePagamento = Console.ReadLine();
-                            switch (controleGastos[contador].formaDePagamento)
+                            controleGasto.formaDePagamento = Console.ReadLine();
+                            switch (controleGasto.formaDePagamento)
                             {
                                 case "1":
-                                    controleGastos[contador].formaDePagamento = "Dinheiro";
+                                    controleGasto.formaDePagamento = "Dinheiro";
                                     formaPagamento = 0;
                                     break;
                                 case "2":
-                                    controleGastos[contador].formaDePagamento = "Cheque";
+                                    controleGasto.formaDePagamento = "Cheque";
                                     formaPagamento = 0;
                                     break;
                                 case "3":
-                                    controleGastos[contador].formaDePagamento = "Cartão";
+                                    controleGasto.formaDePagamento = "Cartão";
                                     formaPagamento = 0;
                                     break;
                                 case "4":
-                                    controleGastos[contador].formaDePagamento = "Cheque pré";
+                                    controleGasto.formaDePagamento = "Cheque pré";
                                     formaPagamento = 0;
                                     break;
                                 default:
@@ -67,30 +69,31 @@ namespace Unidade3.ExercicioComplementar
                                     break;
                             }
                         }
+                        controleGastos.Add(controleGasto);
                         Console.WriteLine("");
-                        contador++;
                         break;
-                    case 2:
+                    case "2":
                         Console.WriteLine("");
-                        Console.WriteLine("------- Listar -------");
+                        Console.WriteLine("--------- Listar ---------");
+                        Console.WriteLine("");
                         if (contador == -1)
                         {
                             Console.WriteLine("Nenhum gasto cadastrado!");
                         }
                         else
                         {
-                            for (int i = 0; i < contador; i++)
+                            foreach (ControleDeGastos listar in controleGastos)
                             {
-                                Console.WriteLine("Tipo de gasto: " + controleGastos[i].tipoDeGasto);
-                                Console.WriteLine("" + controleGastos[contador].dataGasto);
-                                Console.WriteLine("" + controleGastos[contador].valorGasto);
-                                Console.WriteLine("" + controleGastos[contador].formaDePagamento);
+                                Console.WriteLine("Tipo de gasto: " + listar.tipoDeGasto);
+                                Console.WriteLine("Data: " + listar.dataGasto);
+                                Console.WriteLine("Valor gasto: R$ " + listar.valorGasto);
+                                Console.WriteLine("Forma de Pagamento: " + listar.formaDePagamento);
                                 Console.WriteLine("");
                             }
                         }
                         Console.WriteLine("");
                         break;
-                    case 0:
+                    case "0":
                         Console.WriteLine("");
                         Console.WriteLine("Até mais!");
                         Console.WriteLine("");
